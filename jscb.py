1bcc6e74f4825ca97e84737b4d504fe9162fecb0
Mehul Jani <mehul_26@yahoo.co.in>
2025-09-08 15:34:40 -0500

    Update jscb.py

--- jscb.py
+++ jscb.py
@@ -2,10 +2,11 @@
 import sys 
 import os
 import random
+import pandas as pd
 from Bio import SeqIO
 
 if len(sys.argv) == 1:
-	print "\n\nError: Please provide input file in genbank format!!\n\n"
+	print ("\n\nError: Please provide input file in genbank format!!\n\n")
 	sys.exit()
 f = open ("JSCBinput", "w")
 start_col, end_col, rna_start, rna_end, strand_col=[],[],[],[],[]
@@ -16,6 +17,7 @@ for gb_record in SeqIO.parse(sys.argv[1], "genbank"):
 	seq=gb_record.seq
 	seq=str(seq.upper())
 	finseq=""
+    
 	for i in seq:
 		if i in nucs:
 			finseq+=i
@@ -24,10 +26,11 @@ for gb_record in SeqIO.parse(sys.argv[1], "genbank"):
 
 	f.write(finseq)
 	f.close()
+    
 	
 	for feature in gb_record.features:
-		start=feature.location.nofuzzy_start+1
-		end=feature.location.nofuzzy_end
+		start=feature.location.start+1
+		end=feature.location.end
 		if feature.strand == 1:
 			strand ="+"
 		else:
@@ -67,6 +70,8 @@ gicounter=0
 gifile = open("JSCB_output.gi", "r")
 outfile = open("JSCB_output.tsv", "w")
 
+
+
 for line in gifile:
 	if int(line.split()[1])!=int(natclus):
 		if int(line.split()[0]) != prv+1:
@@ -88,11 +93,22 @@ for line in gifile:
 				gicounter+=1
 				if gicounter == 1:
 					outfile.write("Id\tStart\tEnd\n")
-				outfile.write("GI-"+str(gicounter)+"\t"+str(startcoord)+"\t"+str(actend[int(line.split()[0])-2])+"\n")					
+				outfile.write("GI-"+str(gicounter)+"\t"+str(startcoord)+"\t"+str(actend[int(line.split()[0])-2])+"\n")
+
 				
 		gmode = 0
 if gicounter == 0:
 	outfile.write("No Genomic Islands Identified\n")
 outfile.close()
-os.system("rm JSCBinput")
+#os.system("rm JSCBinput")
 #os.system("rm JSCB_coord")
+
+cdf=pd.read_csv("JSCB_coord",sep="\t",names=["strand","start","end"])
+gidf=pd.read_csv("JSCB_output.gi",delim_whitespace=True,names=["num","clus_id","clus_size","len"])
+
+
+df=pd.concat([cdf,gidf], axis=1)
+
+df["clus_label"]=df.apply(lambda x: "A" if int(x["clus_id"])!=int(natclus) else "N", axis=1)
+df=df[["strand","start","end","clus_id","clus_size","clus_label"]]
+df.to_csv("JSCB_clus_info.tsv",sep="\t",index=False)
